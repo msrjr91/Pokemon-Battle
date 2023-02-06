6d123d301becbdb10e730e88bf124bca8f27fb93
Michael <michael.s.ross10@gmail.com>
2023-02-05 22:02:55 -0700

    new commit to merge versions

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 
 
-let userName = 'michael.s.ross10'
+
+
+const selectCard = (res) => {
+  let candidatesList = document.querySelector('.candidates')
+  let candidateArr = []
+  for(i = 0; i < res.data.length; i++){
+    if(res.data[i].id.startsWith('base')){
+      candidateArr.push(res.data[i])
+    }
+  }
+  let candidate = document.createElement('li')
+  candidate.innerHTML = `<img src='${candidateArr[0].images.small}' alt='${res.data[0].name}'>`
+  candidatesList.append(candidate)
+  }
+
 
 async function getData(event){
   event.preventDefault()
 
-  let url = 'https://www.codewars.com/api/v1/users/michael.s.ross10'
+  let textInput = document.querySelector('#inputBar').value
+  const url =  `https://api.pokemontcg.io/v2/cards?q=name:${textInput}`
+
+  // const url = `https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[${i} TO ${i}]`
+  // const url = `https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[1 TO 151]`
 
   fetch(url)
     .then(res => {
       return res.json()
     })
     .then(res => {
-      console.log("success!", res)
-      // let heading = document.querySelector('.title')
-      // heading.innerText = res
+
+      selectCard(res)
+    })
+    .catch(err => {
+      console.log("something went wrong...", err)
     })
 }
 
 let button = document.querySelector('#searchButton')
-// let inputBox = document.querySelector('#inputBar')
-// inputBox.addEventListener("keyup", function(event){
-//   if(event.key !== 'Enter') return;
-//   button.click()
-//   event.preventDefault()
-//   })
-
 button.onclick = getData
